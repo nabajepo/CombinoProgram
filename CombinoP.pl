036e4c0aaeb034196756f7f4e306520417be45ad
nabajepo <nabajepo@gmail.com>
2024-09-25 11:02:00 -0400

    third program

--- CombinoP.pl
+++ CombinoP.pl
@@ -5,6 +5,8 @@
 
 %%%%%%%%%%%%%%%%%%%%% This are all numbers we have to%%%%%%%%%%%%%%%%%
 numbers([0,1,2,3,4,5,6,7,8,9]).
+%Here where we do combinaison
+combinoP(Size,ResL):-numbers(N),Size\=1->combinoManage(N,N,Size,2,ResL),display(ResL),!;ResL=N,display(ResL),!.
 %Here we display result
 display(R):-maplist(writeln,R).
 %Here we make combinaison for N pin(Tour)
