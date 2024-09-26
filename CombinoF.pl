54c602691f7977d45cc572ef5161ca12b52df268
nabajepo <nabajepo@gmail.com>
2024-09-26 11:12:43 -0400

    First Program of CombinoF

--- CombinoF.pl
+++ CombinoF.pl
@@ -1,2 +1,8 @@
 %This program make combinaison and stock them in a txt file
 %Here we start combino
+%Here we add element in back
+pushBack(Element,[],[Element]).
+pushBack(Element,[Tail|Queue],[Tail|NewList]):-pushBack(Element,Queue,NewList).
+%Here we add in-front
+pushFront(Element,List,[Element|List]).
+
