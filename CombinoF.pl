994c06d3ba713ce52960154902d496c1d40d026c
nabajepo <nabajepo@gmail.com>
2024-09-30 09:27:03 -0400

    still working on combinoF.pl

--- CombinoF.pl
+++ CombinoF.pl
@@ -1,5 +1,10 @@
 %This program make combinaison and stock them in a txt file
 %Here we start combino
+%Here we make full combinaison
+combinoComplet([Tail|Queue],LoLs,SListEn,Result):-Queue\=[]->combino(LoLs,Tail,SListEn,TRN)
+                                                           ,combinoComplet(Queue,LoLs,TRN,Result);
+                                                           combino(LoLs,Tail,SListEn,Result),!.
+
 % Here we do combinaison for a specifique element
 combino([Tail|Queue],Elt,LoCres,FRes):-Queue\=[]->pushFront(Elt,Tail,Lres)
                                                  ,pushBack(Lres,LoCres,NLres)
